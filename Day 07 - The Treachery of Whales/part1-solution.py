b9a6bb56aa97ee3e9df9564923264ea57302a3f6
Lars Kecker <lars.kecker2009@gmail.com>
2021-12-07 19:20:02 +0100

    day 07 - part 1

---
+++ Day 07 - The Treachery of Whales/part1-solution.py
@@ -0,0 +1,17 @@
+
+def calc_score(crabs, i):
+    return sum([abs(c - i) for c in crabs])
+
+with open('./input.txt') as input:
+    crabs = [int(s) for s in input.readline().split(',')]
+
+    best_score = max(crabs) * len(crabs)
+
+    for test in range(max(crabs)):
+        new_score = calc_score(crabs, test)
+        if (new_score < best_score):
+            best_score = new_score
+        elif (new_score > best_score):
+            break # it only gets worse from here
+
+    print(best_score) # 355592
